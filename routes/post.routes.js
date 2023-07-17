a74accc8a86b127de3192b16bc97d36172eeb5e2
Prashant_Som <prashantsom75@gmail.com>
2023-07-17 13:26:42 +0530

    all are working

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -18,11 +18,29 @@ postRouter.post("/add",async(req,res)=>{
     }
 })
 postRouter.get("/",async(req,res)=>{
-
+    // const searchqry = req.query.device
+    // const title = new RegExp(searchqry,"i")
     // creatorName:req.name,creatorId:req.userId
     try {
         const post = await PostModel.find({creatorId:req.userId})
         res.status(200).send(post)
+    } catch (error) {
+        res.status(400).send({error:error.message})
+    }
+})
+postRouter.get("/search",async(req,res)=>{
+    const searchqry = req.query.device
+    const title = new RegExp(searchqry,"i")
+    // creatorName:req.name,creatorId:req.userId
+    try {
+        if(!title){
+            const post = await PostModel.find({})
+            res.status(200).send(post)
+        }
+        else{
+            const post = await PostModel.find({device:title})
+            res.status(200).send(post)
+        }
     } catch (error) {
         res.status(400).send({error:error.message})
     }
@@ -45,10 +63,20 @@ postRouter.patch("/update/:id",async(req,res)=>{
         res.status(400).send({error:error.message})
     }
 })
-
-
-
-
+postRouter.delete("/delete/:id",async(req,res)=>{
+    try {
+        const post = await PostModel.findById(req.params.id)
+        if(post.creatorId.toString() !== req.userId){
+            res.status(200).send({msg:"You have to Authorized first"})
+        }
+        else{
+            const deleted = await PostModel.findByIdAndDelete(req.params.id)
+            res.status(200).send({msg:"Post has been Deleted"})
+        }
+    } catch (error) {
+        res.status(400).send({error:error.message})
+    }
+})
 
 
 
